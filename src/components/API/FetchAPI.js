24f464748d4969af118fcc982af0c3bff1bf3e88
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-10-14 05:42:36 -0700

    Update FetchAPI.js

--- src/components/API/FetchAPI.js
+++ src/components/API/FetchAPI.js
@@ -1,11 +1,29 @@
 import React from "react";
 
+function FetchAPI(){
+const apiGet = () => {
+    fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
 
-function FetchAPI() {
-  return( 
+return( 
   <div>
+    My API <br/>
+    <button>Fetch API</button>
   </div>
   );
 }
 
-export default App;
+export default FetchAPI;
+
+
+
+  // async function randomQuote() {
+  //   const response = await fetch('https://api.quotable.io/random')
+  //   const quote = await response.json()
+    
+  //   // Output the quote and author name
+  //   console.log(quote.content)
+  //   console.log(`- ${quote.author}`)
+  // }
