32b3b46d8a2e8a4e892d02a2de6e16b1d7885525
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-11-07 01:20:16 -0800

    Updated fetchapi.js

--- src/components/API/FetchAPI.js
+++ src/components/API/FetchAPI.js
@@ -18,10 +18,10 @@ return(
     <div class = "card">
       <h1>Quote of the Day</h1>
       <p>{JSON.stringify(data.content)}</p>
-      <p class = "authorName">~ {(data.authorSlug.replace(/-/g, " "))}</p>
+      <p class = "authorName">~ {(data.authorSlug)}</p>
       <br/>
       <br/>
-      <button onClick={apiGet}>Next Quote</button>
+      <button onClick={apiGet}>Get Quote</button>
 
     </div>
   </div>
