258e3c689072e3f0c31650377953f4d7454eb4d6
Nayeli De Jesus <nayelidjm142@gmail.com>
2023-12-06 23:00:44 -0800

    Update FetchAPI.js
    added useEffect()

--- src/components/API/FetchAPI.js
+++ src/components/API/FetchAPI.js
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import '../../FetchAPI.css';
 
 function FetchAPI(){
-  const [data, setData] = useState([]);
+  const [quote, setQuotes] = useState([]);
 const apiGet = () => {
     fetch('https://api.quotable.io/random')
     .then((response) => response.json())
     .then((json) => {
       console.log(json); 
-      setData(json);
+      setQuotes(json);
     });
 
   
 };
+useEffect(() => {
+  apiGet();
+}, []);
 
 return( 
   <div>
     <div class = "card">
       <h1>Quote of the Day</h1>
-      <p>{JSON.stringify(data.content)}</p>
-      <p class = "authorName"> {(data.author)}</p>
+      <p>{JSON.stringify(quote.content)}</p>
+      <p class = "authorName"> {(quote.author)}</p>
       <br/>
       <br/>
       <button onClick={apiGet}>Get Quote</button>
@@ -30,4 +33,3 @@ return(
 }
 
 export default FetchAPI;
-
